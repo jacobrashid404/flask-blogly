ef1ef11de92580cbc2dbe36c6fa8bba808ad8721
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-01 18:40:28 -0400

    Set up configuration for app.

--- blogly/app.py
+++ blogly/app.py
@@ -2,8 +2,19 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug import NotFound
+
+from models import db, dbx, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
+app.config['SQLALCHEMY_ECHO'] = True
+db.init_app(app)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "i-will-never-tell")
+print("secret key=", app.config['SECRET_KEY'])
+
+debug = DebugToolbarExtension(app)
