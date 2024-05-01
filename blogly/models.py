a3e7992164ed61fd76af240d9da63bb23428a14e
Jacob <jacobrashid99@gmail.com>
2024-05-01 12:29:29 -0700

    models

--- blogly/models.py
+++ blogly/models.py
@@ -1,1 +1,30 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+dbx = db.session.execute
+
+class User(db.Model):
+    """User model"""
+    
+    __tablename__ = "users"
+    
+    id = db.mapped_column(
+        db.Integer,
+        db.Identity(),
+        primary_key=True,
+    )
+    
+    first_name = db.mapped_column(
+        db.String(50),
+        nullable=False,
+    )
+    
+    last_name = db.mapped_column(
+        db.String(50),
+    )
+    
+    img_url = db.mapped_column(
+        db.String(500)
+    )
