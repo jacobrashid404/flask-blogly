341ba66861fa4644f6a049120fb876e02f91e4c1
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-01 18:26:18 -0400

    Wrote method for get_full_name().

--- blogly/models.py
+++ blogly/models.py
@@ -5,26 +5,32 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 dbx = db.session.execute
 
+
 class User(db.Model):
     """User model"""
-    
+
     __tablename__ = "users"
-    
+
     id = db.mapped_column(
         db.Integer,
         db.Identity(),
         primary_key=True,
     )
-    
+
     first_name = db.mapped_column(
         db.String(50),
         nullable=False,
     )
-    
+
     last_name = db.mapped_column(
         db.String(50),
     )
-    
+
     img_url = db.mapped_column(
         db.String(500)
     )
+
+    def get_full_name(self):
+        """ Generate full name from user's first and last names."""
+
+        return f"{self.first_name} {self.last_name}"
