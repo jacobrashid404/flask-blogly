154ffdc26145607170826ae29a7473e2a2969eb9
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-01 20:52:50 -0400

    Updated routes for showing user detail and for editing user detail.

--- blogly/app.py
+++ blogly/app.py
@@ -19,28 +19,32 @@ print("secret key=", app.config['SECRET_KEY'])
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get("/")
 def redirect_to_users():
     """Redirect to user list"""
-    
+
     return redirect("/users")
+
 
 @app.get("/users")
 def show_user_list():
     """Show user list"""
-    
+
     return render_template("users.jinja")
+
 
 @app.get("/users/new")
 def show_add_user_form():
     return render_template("new_user_form.jinja")
+
 
 @app.post("/users/new")
 def add_user_from_form():
     first_name = request.form['first_name']
     last_name = request.form['last_name'] or None
     img_url = request.form['user_image'] or None
-   
+
     user = User(
         first_name=first_name,
         last_name=last_name,
@@ -48,5 +52,23 @@ def add_user_from_form():
     )
     db.session.add(user)
     db.session.commit()
-    
+
     return redirect("/users")
+
+
+@app.get("/users/<int:user_id>")
+def show_user_detail(user_id):
+    """ Show information about given user. """
+
+    # query db for user instance's id
+
+    #
+
+    return render_template("user_detail.jinja", )
+
+
+@app.get("/users/<int:user_id>/edit")
+def show_edit_page(user_id):
+    """ Show the edit page for a user. """
+
+    return render_template("edit_profile.jinja", )
